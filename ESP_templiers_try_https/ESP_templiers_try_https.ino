9b5ce518a462071f42de444f3f77ca41bba3129d
U-GRIDPOCKET\pape <niamadio>
2016-01-06 13:57:48 +0100

    Just changed the header

--- ESP_templiers_try_https/ESP_templiers_try_https.ino
+++ ESP_templiers_try_https/ESP_templiers_try_https.ino
@@ -1,6 +1,4 @@
 /*
- *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
- *
  *  You need to get streamId and privateKey at data.sparkfun.com and paste them
  *  below. Or just customize this script to talk to other HTTP servers.
  *
